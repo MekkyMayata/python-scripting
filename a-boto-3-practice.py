336b3c01b817d5268b29099297038885a33d2a92
mekkymayata <okpallannaemeka@gmail.com>
2022-06-02 17:47:25 -0500

    update: handle file existence

--- a-boto-3-practice.py
+++ a-boto-3-practice.py
@@ -19,7 +19,11 @@ logger1 = logging.getLogger('file_logger')
 
 
 def s3_pull() -> list:
-    # get current s3 buckets and append them to a file
+    """
+    pulls existing s3 buckets and
+    appends result to a list
+    :return: list
+    """
     s3 = boto3.resource('s3')
     s3_buckets = []
     for bucket in s3.buckets.all():
@@ -28,16 +32,33 @@ def s3_pull() -> list:
 
 
 def create_file(filename: str) -> None:
+    """
+    creates a file and appends
+    s3 bucket names
+    :param filename: str
+    :return: None
+    """
     file = filename
     logger1.debug('FILE LOCATION: {}'.format(file))
 
-    # create file if NOT exist
+    # file does NOT exist
     if not os.path.isfile(file):
         logger1.info('FILE DOES NOT EXIST, CREATING...')
-        with open(file, 'a') as f:
+        with open(file, 'w') as f:
             for i in s3_pull():
                 f.write(i + '\n')
         logger1.info('WRITE(S) COMPLETE.')
+    else:
+        logger1.info('FILE EXISTS')
+        with open(file, 'r+') as f:
+            # file is EMPTY
+            if os.path.getsize(file) == 0:
+                for i in s3_pull():
+                    f.write(i + '\n')
+            elif list(f)[-1].split('\n')[0] != s3_pull()[-1]:
+                for i in s3_pull():
+                    f.write(i + '\n')
+    logger1.info('WRITE(S) COMPLETE.')
 
 
 if __name__ == '__main__':
